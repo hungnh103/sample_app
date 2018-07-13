f9e9a88967f54e2333153c3f7d4f2341b83ea6c4
Tuyendk4 <tuyenluuminh@gmail.com>
2018-07-13 20:11:49 +0700

    Delete hello method and translate content of method to Vietnamese

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  def hello
-    render html: "Hello, world!"
-  end
-
-  def logged_in_user
-    unless logged_in?
+  def nguoi_dung_da_dang_nhap
+    unless da_dang_nhap?
       store_location
       flash[:danger] = "Please log in."
       redirect_to login_url
